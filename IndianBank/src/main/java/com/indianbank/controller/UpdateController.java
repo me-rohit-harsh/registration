d92a922da12af5c215397275326f28067a242e97
Rohit kumar <rohitkumarah369@gmail.com>
2023-09-14 06:40:27 +0530

    Security for withdrawal

--- IndianBank/src/main/java/com/indianbank/controller/UpdateController.java
+++ IndianBank/src/main/java/com/indianbank/controller/UpdateController.java
@@ -95,27 +95,42 @@ public class UpdateController {
 			return "redirect:index?deposited";
 		}
 		session.setAttribute("msg", false);
-		session.setAttribute("errormsg", "Oops Something went wrong!");
+		session.setAttribute("errormsg", "Oops!! Something went wrong!");
 		return "redirect:index?error";
 	}
 
 	@PostMapping("/withdrawl")
 	public String withdrawl(@ModelAttribute("user") User user, HttpSession session) {
 		User getUser = (User) session.getAttribute("user");
-		if (getUser.getBalance() > user.getBalance()) {
-			getUser.setBalance(getUser.getBalance() - user.getBalance());
-			session.setAttribute("newBalance", getUser.getBalance());
-			userService.saveUser(getUser);
-			session.setAttribute("withdraw", true);
-			session.setAttribute("withdrawMessage", "Money has been debited!!  ");
-			return "redirect:/index?withdrawn";
+		if (getUser.getPassword().equals(user.getPassword())) {
+			if (getUser.getBalance() > user.getBalance()) {
+				getUser.setBalance(getUser.getBalance() - user.getBalance());
+				session.setAttribute("newBalance", getUser.getBalance());
+				userService.saveUser(getUser);
+				session.setAttribute("withdraw", true);
+				session.setAttribute("withdrawMessage", "Money has been debited!!  ");
+				return "redirect:/index?withdrawn";
+			}
+			session.setAttribute("msg", false);
+			session.setAttribute("errormsg", "Opps! Insufficient funds");
+			return "redirect:index?error";
+		} else {
+			session.setAttribute("msg", false);
+			session.setAttribute("errormsg", "Opps! Incorrect Password");
+			return "redirect:index?error";
 		}
 
-		session.setAttribute("msg", false);
-		session.setAttribute("errormsg", "Opps! Insufficient funds");
-		return "redirect:index?error";
 	}
 //	I am able to get the authenticated user and send them back to the view
 //	and now what i have to do is to create update page
 //	and write back end code to save the newly data in DB
+//	i have implemented deposit and withdrawal functionalities
+	/*
+	 * now i have to implement send money so for that i need to create a new page
+	 * where i will take input of receiver userId as UPI Id. and will provide
+	 * function to check whether that user exists or not if yes then will display
+	 * the name of the user. and then will check that balance entered is less or
+	 * equal than the total fund, and then authentication and then finally sent
+	 */
+	
 }
